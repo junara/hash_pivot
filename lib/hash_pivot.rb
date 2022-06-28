cdd7a62a2c16445e1e3f225261990e0a7cc39ac9
junara <jun5araki@gmail.com>
2022-06-28 09:38:38 +0900

    refactoring

--- lib/hash_pivot.rb
+++ lib/hash_pivot.rb
@@ -4,12 +4,28 @@ require_relative 'hash_pivot/version'
 require_relative 'hash_pivot/table'
 require 'hash_pivot/error/not_implemented_error'
 module HashPivot
+  # rubocop:disable Layout/LineLength
+
   # @param [Array<Hash>] data
   # @param [Array] group
   # @param [Object] pivot_in
-  # @param [Array] pivot_kinds
+  # @param [Array,Hash,nil] pivot_header
   # @return [Hash]
-  def self.pivot(data, group, pivot_in, pivot_kinds, repository: HashPivot::Repository::HashRepository, &block)
+  # @param [Class<HashPivot::Repository::HashRepository,HashPivot::Repository::StructRepository,HashPivot::Repository::ActiveRecordRepository>] repository
+  # @param [Proc] block
+  def self.pivot(data, group, pivot_in, pivot_header, repository: HashPivot::Repository::HashRepository, &block)
+    pivot_kinds = if pivot_header.is_a?(Array)
+                    transform_to_hash(pivot_header)
+                  else
+                    pivot_header
+                  end
     Table.new(data, repository: repository).pivot(group, pivot_in, pivot_kinds, &block)
   end
+
+  # rubocop:enable Layout/LineLength
+
+  def self.transform_to_hash(array)
+    array.each_with_object({}) { |kind, memo| memo[kind] = kind }
+  end
+  private_class_method :transform_to_hash
 end
--- lib/hash_pivot/table.rb
+++ lib/hash_pivot/table.rb
@@ -6,19 +6,26 @@ require_relative './repository/struct_repository'
 
 module HashPivot
   class Table
+    # rubocop:disable Layout/LineLength
+
     # @param [Array<Hash>] data
+    # @param [Class<HashPivot::Repository::HashRepository,HashPivot::Repository::StructRepository,HashPivot::Repository::ActiveRecordRepository>] repository
     def initialize(data, repository: HashPivot::Repository::HashRepository)
       @data = data
       @repository = repository
     end
 
+    # rubocop:enable Layout/LineLength
+
     # @param [Array, Object] group
     # @param [Object] pivot_in
-    # @param [Array] pivot_kinds
+    # @param [Hash,nil] pivot_kinds
+    # @param [Proc] block
+    # @return [Array<Hash>]
     def pivot(group, pivot_in, pivot_kinds, &block)
       group = [group] unless group.is_a?(Array)
       @repository.hash_by_group(@data, group, pivot_in).each_with_object([]) do |(key, array), memo|
-        hash = pivot_with_sum(pivot_kinds, array, pivot_in, &block)
+        hash = transpose_with(pivot_kinds, array, pivot_in, &block)
 
         memo << key.merge(hash)
       end
@@ -26,43 +33,24 @@ module HashPivot
 
     private
 
-    # @param [Array,Hash] pivot_kinds
+    # @param [Hash,nil] pivot_kinds
     # @param [Array] array
     # @param [Object] pivot_in
     # @param [Proc] block
     # @return [Hash]
-    def pivot_with_sum(pivot_kinds, array, pivot_in, &block)
-      pivot_kinds ||= array.map { |h| h[pivot_in] }.uniq.compact
-      case pivot_kinds
-      when Array
-        pivot_with_sum_with_array_pivot_kinds(pivot_kinds, array, pivot_in, &block)
-      when Hash
-        pivot_with_sum_with_hash_pivot_kinds(pivot_kinds, array, pivot_in, &block)
-      end
-    end
-
-    # @param [Array,Hash] pivot_kinds
-    # @param [Array] array
-    # @param [Object] pivot_in
-    # @param [Proc] block
-    # @return [Hash]
-    def pivot_with_sum_with_array_pivot_kinds(pivot_kinds, array, pivot_in, &block)
-      pivot_kinds.each_with_object({}) do |pivot_kind, memo|
-        pivoted_data = array.select { |h| h[pivot_in] == pivot_kind }
-        memo[pivot_kind] = block ? yield(pivoted_data) : pivoted_data
-      end
-    end
-
-    # @param [Array,Hash] pivot_kinds
-    # @param [Array] array
-    # @param [Object] pivot_in
-    # @param [Proc] block
-    # @return [Hash]
-    def pivot_with_sum_with_hash_pivot_kinds(pivot_kinds, array, pivot_in, &block)
+    def transpose_with(pivot_kinds, array, pivot_in, &block)
+      pivot_kinds ||= calculated_pivot_kinds_from(array, pivot_in)
       pivot_kinds.each_with_object({}) do |(pivot_kind, pivot_label), memo|
         pivoted_data = array.select { |h| h[pivot_in] == pivot_kind }
         memo[pivot_label] = block ? yield(pivoted_data) : pivoted_data
       end
+    end
+
+    # @param [Array] array
+    # @param [Object] pivot_in
+    # @return [Hash]
+    def calculated_pivot_kinds_from(array, pivot_in)
+      array.map { |h| h[pivot_in] }.uniq.compact.each_with_object({}) { |kind, memo| memo[kind] = kind }
     end
   end
 end
